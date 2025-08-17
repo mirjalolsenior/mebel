ada9603da723cd8ddfad4ce5ed990b86549375d3
mirjalolsenior <amontagayev@gmail.com>
2025-08-18 04:57:04 +0500

    s

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  output: 'export',
+  distDir: '.next',
+  experimental: {
+    esmExternals: false,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://raw.githack.com;",
+          },
+        ],
+      },
+    ]
+  },
+}
